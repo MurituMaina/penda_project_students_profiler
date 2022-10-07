3eefe0f8e664f5f8ec7a612e63a3065170a95426
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-07 21:51:01 +0300

    refactor order code

--- src/Footer.js
+++ src/Footer.js
@@ -4,47 +4,34 @@ const Footer = () => {
   const date = new Date().getFullYear();
   return (
     <div>
-    <div className="footer">
-      <div>
-        <h2>Contacts</h2>
-        <h3>Director</h3>
-        <a href="mailto:vincentmaina0@gmail.com">vincentmaina0@gmail.com</a>
-        <p>+2547123456789</p>
-        <h3>Director</h3>
-        <p>+2547123456789</p>
-        <h3>Director</h3>
-        <p>+2547123456789</p>
-        <p>More</p>
-      </div>
+      <div className="footer">
+        <div>
+          <h2>Contact</h2>
+          <h3>Director</h3>
+          <a href="mailto:vincentmaina0@gmail.com">vincentmaina0@gmail.com</a>
+          <p>+2547123456789</p>
 
-      <div>
-        <h2>Contacts</h2>
-        <h3>Director</h3>
-        <a href="mailto:vincentmaina0@gmail.com">vincentmaina0@gmail.com</a>
-        <p>+2547123456789</p>
-        <h3>Director</h3>
-        <p>+2547123456789</p>
-        <h3>Director</h3>
-        <p>+2547123456789</p>
-        <p>More</p>
-      </div>
+          <p>More</p>
+        </div>
 
-      <div>
-        <h2>Contacts</h2>
-        <h3>Director</h3>
-        <a href="mailto:vincentmaina0@gmail.com">vincentmaina0@gmail.com</a>
-        <p>+2547123456789</p>
-        <h3>Director</h3>
-        <p>+2547123456789</p>
-        <h3>Director</h3>
-        <p>+2547123456789</p>
-        <p>More</p>
-      </div>
-      </div>
-<div className="date">
-&copy; {date}
+        <div>
+          <h2>Contacts</h2>
+          <h3>Manager</h3>
+          <a href="mailto:addymwangi@gmail.com">addymwangi@gmail.com</a>
+          <p>+2547123456789</p>
+          <p>More</p>
+        </div>
 
-</div>
+        <div>
+          <h2>Contacts</h2>
+          <h3>Feedback</h3>
+          <a href="mailto:feedback@penda.com">feedback@penda.com</a>
+          <p>+2547187654321</p>
+
+          <p>More</p>
+        </div>
+      </div>
+      <div className="date">&copy; {date}</div>
     </div>
   );
 };
