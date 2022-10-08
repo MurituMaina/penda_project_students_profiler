176e893610991041b36bbc850164397d3a4a43fe
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-08 10:33:36 +0300

    add logo image and link to home page

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import penda from './penda.jpeg'
+
 
 function Navbar() {
   return (
     <div className="links navbar">
       <ul>
+      <li>
+        <NavLink to="/"><img alt="logo" src={penda}/></NavLink>
+      </li>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
