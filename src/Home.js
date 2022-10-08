62630f35fc9114aab6cb5ce6698a804712ad91a8
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-08 11:41:45 +0300

    add the SlideShow component in the home and the rotating wheel

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,29 @@
 import React from "react";
+import SlideShow from "./SlideShow";
+import wheel from "./images/wheel.png";
+import animal from "./images/animal.jpeg";
 
-function Home() {
+function Home({ verses }) {
   return (
     <div>
-    <div className="home">
-    
-      <h1>Penda project Students</h1>
-      
-      <p>
-        Kibera Penda Project has been around for over a decade. Over this period
-        many students who went to New Hope Kibera School have had the
-        opportunity to go to through school through the suport of well wishers.
-      </p>
-      <p>
-        Over time, many have gone ahead and can be able to track their Status in
-        terms of progress and what they engage with and where they are in school
-        or even work.
-      </p>
-      
+      <div className="home">
+        <h1>Penda project Students</h1>
+
+        <p>
+          Kibera Penda Project has been around for over a decade. Over this
+          period many students who went to New Hope Kibera School have had the
+          opportunity to go to through school through the suport of well
+          wishers.
+        </p>
+        <p>
+          Over time, many have gone ahead and can be able to track their Status
+          in terms of progress and what they engage with and where they are in
+          school or even work.
+        </p>
+        <img alt="wheel" src={wheel} className="wheel" />
+      </div>
+      <div>
+        <SlideShow verses={verses} />
       </div>
     </div>
   );
